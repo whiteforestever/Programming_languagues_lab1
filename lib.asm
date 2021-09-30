02e53fec2163130b04eafd6de25214764cd8731a
whiteforestever <danila.whiteman@gmail.com>
2021-09-30 17:36:36 +0300

    First v

---
+++ lib.asm
@@ -0,0 +1,130 @@
+section .data
+message: db  'hello, world!', 10
+fileName: db 'It is very simple', 0
+newline_char: db 10
+codes: db '0123456789abcdef'
+
+section .text
+ 
+ 
+; Принимает код возврата и завершает текущий процесс
+exit:
+    mov     rax, 60; 
+    xor     rdi, rdi
+    syscall
+
+; Принимает указатель на нуль-терминированную строку, выводит её в stdout
+print_string_null_terminated:
+    push rdi
+    call string_length
+    pop rsi
+    mov rdx, rax
+    mov rax, 1
+    mov rdi, 1
+    syscall
+    ret
+
+; Принимает указатель на нуль-терминированную строку, возвращает её длину
+string_length:
+    xor rax, rax
+.loop:
+    cmp byte [rdi+rax], 0
+    je .end
+    inc rax
+    jmp .loop
+.end:
+    ret
+
+; Принимает код символа и выводит его в stdout
+print_char:
+    push rdi
+    mov rdx, 1
+    mov rax, 1
+    mov rsi, rsp 
+    mov rdi, 1
+    syscall
+    pop rdi
+    ret 
+
+; Переводит строку (выводит символ с кодом 0xA)
+print_newline:
+    print_newline:
+    mov rax, 1            ; 'write' syscall identifier
+    mov rdi, 1            ; stdout file descriptor
+    mov rsi, newline_char ; where do we take data from
+    mov rdx, 1            ; the amount of bytes to write
+    syscall
+   ret
+
+; Выводит беззнаковое 8-байтовое число в десятичном формате 
+; Совет: выделите место в стеке и храните там результаты деления
+; Не забудьте перевести цифры в их ASCII коды.
+print_uint:
+    xor rax, rax
+    ret
+
+; Выводит знаковое 8-байтовое число в десятичном формате 
+print_int:
+    xor rax, rax
+    ret
+
+; Принимает два указателя на нуль-терминированные строки, возвращает 1 если они равны, 0 иначе
+string_equals:
+    xor rax, rax
+    ret
+
+; Читает один символ из stdin и возвращает его. Возвращает 0 если достигнут конец потока
+read_char:
+    mov rax, 0     
+    mov rdi, 0     
+    push 0         
+    mov rsi, rsp  
+    mov rdx, 1     
+    syscall
+    pop rax
+    cmp rax, 10
+    jz .A
+    jmp .RET
+.A:
+    mov rax, 48
+.RET:
+    ret 
+
+; Принимает: адрес начала буфера, размер буфера
+; Читает в буфер слово из stdin, пропуская пробельные символы в начале, .
+; Пробельные символы это пробел 0x20, табуляция 0x9 и перевод строки 0xA.
+; Останавливается и возвращает 0 если слово слишком большое для буфера
+; При успехе возвращает адрес буфера в rax, длину слова в rdx.
+; При неудаче возвращает 0 в rax
+; Эта функция должна дописывать к слову нуль-терминатор
+
+read_word:
+    ret
+ 
+
+; Принимает указатель на строку, пытается
+; прочитать из её начала беззнаковое число.
+; Возвращает в rax: число, rdx : его длину в символах
+; rdx = 0 если число прочитать не удалось
+parse_uint:
+    xor rax, rax
+    ret
+
+
+
+
+; Принимает указатель на строку, пытается
+; прочитать из её начала знаковое число.
+; Если есть знак, пробелы между ним и числом не разрешены.
+; Возвращает в rax: число, rdx : его длину в символах (включая знак, если он был) 
+; rdx = 0 если число прочитать не удалось
+parse_int:
+    xor rax, rax
+    ret 
+
+; Принимает указатель на строку, указатель на буфер и длину буфера
+; Копирует строку в буфер
+; Возвращает длину строки если она умещается в буфер, иначе 0
+string_copy:
+    xor rax, rax
+    ret
